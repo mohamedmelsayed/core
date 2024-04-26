7d137eb3a3d5371df93f5f2326158993181d9a6d
Mohamed <mohamedosmanelsayed@yahoo.com>
2024-04-26 21:50:10 +0200

    fillables

--- app/Models/ContentTranslation.php
+++ app/Models/ContentTranslation.php
@@ -10,4 +10,8 @@ use Illuminate\Database\Eloquent\Model;
 class ContentTranslation extends Model {
     use GlobalStatus, Searchable, ApiQuery;
 
+    $fillables=[
+        'translated_tags','translated_description','translated_title','translated_keywords','type '
+    ]
+
 }
