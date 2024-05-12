6f77f17bb5af87a3caa2ce576cbab311ae1cded8
Mohamed <mohamedosmanelsayed@yahoo.com>
2024-05-12 11:47:27 +0200

    upload audio

--- resources/views/admin/partials/sidenav.blade.php
+++ resources/views/admin/partials/sidenav.blade.php
@@ -58,13 +58,13 @@
                                 </a>
                             </li>
                             <li class="sidebar-menu-item {{ menuActive(['admin.item.audio']) }}">
-                                <a class="nav-link" href="{{ route('admin.item.audio') }}">
+                                <a class="nav-link" href="{{ route('admin.item.uploadAudio') }}">
                                     <i class="menu-icon las la-dot-circle"></i>
                                     <span class="menu-title">@lang('Audio Items')</span>
                                 </a>
                             </li>
                             <li class="sidebar-menu-item {{ menuActive(['admin.item.audio.episode']) }}">
-                                <a class="nav-link" href="{{ route('admin.item.audio.episode') }}">
+                                <a class="nav-link" href="{{ route('admin.item.uploadAudio') }}">
                                     <i class="menu-icon las la-dot-circle"></i>
                                     <span class="menu-title">@lang('Audio Episode Items')</span>
                                 </a>
