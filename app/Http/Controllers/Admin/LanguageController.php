0a044a00c6fe338216b3149e3688cbb4f4a093f2
wali-eldin-hassan <creaspo6@gmail.com>
2024-05-21 06:27:26 +0300

    debug

--- app/Http/Controllers/Admin/LanguageController.php
+++ app/Http/Controllers/Admin/LanguageController.php
@@ -360,8 +360,10 @@ class LanguageController extends Controller
 
     public function showTranslationForm($type, $id)
     {
-        $pageTitle = 'Language Manager';
+
         dd([$type, $id]);
+
+        $pageTitle = 'Language Manager';
         // Fetch the reference video based on type and id
         if ($type === 'video'|| $type==='audio') {
             $reference = Item::findOrFail($id);
