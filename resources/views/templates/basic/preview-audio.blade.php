3040c1483acdb4703892309c82585a87810e3190
wali-eldin-hassan <creaspo6@gmail.com>
2024-06-03 15:33:42 +0300

    wip

--- resources/views/templates/basic/preview-audio.blade.php
+++ resources/views/templates/basic/preview-audio.blade.php
@@ -6,14 +6,14 @@
                 <div class="col-xl-8 col-lg-8 mb-30">
                     <div class="movie-item">
                         <div class="main-video">
-                            <video class="video-player plyr-video" playsinline controls data-poster="{{ getImage(getFilePath('item_landscape') . '/' . $item->image->landscape) }}">
+                            <audio class="video-player plyr-video"  controls data-poster="{{ getImage(getFilePath('item_landscape') . '/' . $item->image->landscape) }}">
                                 @foreach ($audios as $audio)
-                                    <source src="{{ $audio->content }}" type="mp3" size="{{ $audio->size }}" />
+                                    <source src="{{ $audio->content }}" type="audio/mp3" size="{{ $audio->size }}" />
                                 @endforeach
                                 @foreach ($subtitles ?? [] as $subtitle)
                                     <track kind="captions" label="{{ $subtitle->language }}" src="{{ getImage(getFilePath('subtitle') . '/' . $subtitle->file) }}" srclang="{{ $subtitle->code }}" />
                                 @endforeach
-                            </video>
+                            </audio>
                             @if ($item->version == Status::RENT_VERSION && !$watchEligable)
                                 <div class="main-video-lock">
                                     <div class="main-video-lock-content">
@@ -28,7 +28,7 @@
                             @endif
                         </div>
                         <div class="ad-video position-relative d-none">
-                            <video class="ad-player" style="display: none" id="ad-video"></video>
+                            <audio class="ad-player" style="display: none" id="ad-video"></audio>
                             <div class="ad-links d-none">
 {{--                                @foreach ($adsTime ?? [] as $ads)--}}
 {{--                                    <source src="{{ $ads }}" type="video/mp4" />--}}
@@ -433,7 +433,7 @@
                             type: 'audio',
                             sources: [{
                                 src: $('.ad-links').children('source').eq(i).attr('src'),
-                                type: 'audio/mp4'
+                                type: 'audio/mp3'
                             }],
                             poster: "{{ getImage(getFilePath('item_landscape') . '/' . $item->image->landscape) }}",
                         };
@@ -534,7 +534,7 @@
 
 
             function initData() {
-                const video = document.querySelector('video');
+                const video = document.querySelector('audio');
                 $.each(data, function() {
                     if (!Hls.isSupported()) {
                         video.src = this.src;
