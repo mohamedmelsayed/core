e98864e2299850991aabb51007d42ba85f6ac1fb
wali-eldin-hassan <creaspo6@gmail.com>
2024-06-03 11:42:51 +0300

    wip

--- app/Http/Helpers/helpers.php
+++ app/Http/Helpers/helpers.php
@@ -478,7 +478,6 @@ function getAudioFile($audio) {
     }
     if ($server == Status::CURRENT_SERVER) {
         $videoFile = asset($content);
-        dd($videoFile);
     } else if ($server == Status::FTP_SERVER) {
         $general   = gs();
         $videoFile = $general->ftp->domain . '/' . $content;
