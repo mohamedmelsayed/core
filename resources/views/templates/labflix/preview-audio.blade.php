ce729df505067e48e6818b5c5c58aca75b846f31
Mohamed <mohamedosmanelsayed@yahoo.com>
2024-06-04 21:17:25 +0300

    try update preview

--- resources/views/templates/labflix/preview-audio.blade.php
+++ resources/views/templates/labflix/preview-audio.blade.php
@@ -4,10 +4,12 @@
         <div class="container">
             <div class="row @if (blank($episodes)) justify-content-center @endif mb-30-none">
                 <div class="col-xl-8 col-lg-8 mb-30">
-                    <div class="audio-item">
+                    <div class="audio-item" style="position: relative; overflow: hidden;">
+                        <!-- Background Image and Mask -->
+                        <div class="audio-background" style="background-image: url('{{ getImage(getFilePath('item_landscape') . '/' . $item->image->landscape) }}');"></div>
+                        <div class="audio-mask"></div>
                         <div class="main-audio">
-                            <audio class="audio-player plyr-audio" controls controlsList="nodownload"
-                                   data-poster="{{ getImage(getFilePath('item_landscape') . '/' . $item->image->landscape) }}">
+                            <audio class="audio-player plyr-audio" controls controlsList="nodownload">
                                 @foreach ($audios as $audio)
                                     <source src="{{ $audio->content }}" type="audio/mp3"/>
                                 @endforeach
@@ -18,26 +20,22 @@
                                         <span class="icon"><i class="las la-lock"></i></span>
                                         <p class="title">@lang('Purchase Now')</p>
                                         <p class="price">
-                                            <span
-                                                class="price-amount">{{ $general->cur_sym }}{{ showAmount($item->rent_price) }}</span>
-                                            <span
-                                                class="small-text ms-3">@lang('For') {{ $item->rental_period }} @lang('Days')</span>
+                                            <span class="price-amount">{{ $general->cur_sym }}{{ showAmount($item->rent_price) }}</span>
+                                            <span class="small-text ms-3">@lang('For') {{ $item->rental_period }} @lang('Days')</span>
                                         </p>
                                     </div>
                                 </div>
                             @endif
                         </div>
                         <div class="audio-content">
-                            <div
-                                class="audio-content-inner d-sm-flex justify-content-between align-items-center flex-wrap">
+                            <div class="audio-content-inner d-sm-flex justify-content-between align-items-center flex-wrap">
                                 <div class="audio-content-left">
                                     <h3 class="title">{{ __($seoContents["social_title"]) }}</h3>
-                                    <span class="sub-title">@lang('Category') : <span
-                                            class="cat">{{ @$item->category->name }}</span>
+                                    <span class="sub-title">@lang('Category') : <span class="cat">{{ @$item->category->name }}</span>
                                     @if ($item->sub_category)
                                             @lang('Sub Category'): {{ @$item->sub_category->name }}
                                         @endif
-                                </span>
+                                    </span>
                                 </div>
                                 <div class="audio-content-right">
                                     <div class="audio-widget-area align-items-center">
@@ -50,35 +48,26 @@
                                             @endif
                                         @endauth
                                         <span class="audio-widget"><i class="lar la-star text--warning"></i> {{ getAmount($item->ratings) }}</span>
-                                        <span class="audio-widget"><i
-                                                class="lar la-eye text--danger"></i> {{ getAmount($item->view) }} @lang('views')</span>
+                                        <span class="audio-widget"><i class="lar la-eye text--danger"></i> {{ getAmount($item->view) }} @lang('views')</span>
                                         @php
                                             $wishlist = $item->wishlists->where('user_id', auth()->id())->count();
                                         @endphp
-                                        <span class="audio-widget addWishlist {{ $wishlist ? 'd-none' : '' }}"
-                                              data-id="{{ $item->id }}" data-type="item"><i
-                                                class="las la-plus-circle"></i></span>
-                                        <span class="audio-widget removeWishlist {{ $wishlist ? '' : 'd-none' }}"
-                                              data-id="{{ $item->id }}" data-type="item"><i
-                                                class="las la-minus-circle"></i></span>
+                                        <span class="audio-widget addWishlist {{ $wishlist ? 'd-none' : '' }}" data-id="{{ $item->id }}" data-type="item"><i class="las la-plus-circle"></i></span>
+                                        <span class="audio-widget removeWishlist {{ $wishlist ? '' : 'd-none' }}" data-id="{{ $item->id }}" data-type="item"><i class="las la-minus-circle"></i></span>
                                     </div>
                                     <ul class="post-share d-flex align-items-center justify-content-sm-end mt-2 flex-wrap">
                                         <li class="caption">@lang('Share') :</li>
                                         <li data-bs-toggle="tooltip" data-bs-placement="top" title="@lang('Facebook')">
-                                            <a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode(url()->current()) }}"><i
-                                                    class="lab la-facebook-f"></i></a>
+                                            <a href="https://www.facebook.com/sharer/sharer.php?u={{ urlencode(url()->current()) }}"><i class="lab la-facebook-f"></i></a>
                                         </li>
                                         <li data-bs-toggle="tooltip" data-bs-placement="top" title="@lang('Linkedin')">
-                                            <a href="http://www.linkedin.com/shareArticle?mini=true&amp;url={{ urlencode(url()->current()) }}&amp;title={{ __(@$item->title) }}&amp;summary=@php echo strLimit(strip_tags($item->description), 130); @endphp"><i
-                                                    class="lab la-linkedin-in"></i></a>
+                                            <a href="http://www.linkedin.com/shareArticle?mini=true&amp;url={{ urlencode(url()->current()) }}&amp;title={{ __(@$item->title) }}&amp;summary=@php echo strLimit(strip_tags($item->description), 130); @endphp"><i class="lab la-linkedin-in"></i></a>
                                         </li>
                                         <li data-bs-toggle="tooltip" data-bs-placement="top" title="@lang('Twitter')">
-                                            <a href="https://twitter.com/intent/tweet?text={{ __(@$item->title) }}%0A{{ url()->current() }}"><i
-                                                    class="lab la-twitter"></i></a>
+                                            <a href="https://twitter.com/intent/tweet?text={{ __(@$item->title) }}%0A{{ url()->current() }}"><i class="lab la-twitter"></i></a>
                                         </li>
                                         <li data-bs-toggle="tooltip" data-bs-placement="top" title="@lang('Pinterest')">
-                                            <a href="http://pinterest.com/pin/create/button/?url={{ urlencode(url()->current()) }}&description={{ __(@$item->title) }}&media={{ getImage(getFilePath('item_landscape') . '/' . @$item->image->landscape) }}"><i
-                                                    class="lab la-pinterest"></i></a>
+                                            <a href="http://pinterest.com/pin/create/button/?url={{ urlencode(url()->current()) }}&description={{ __(@$item->title) }}&media={{ getImage(getFilePath('item_landscape') . '/' . @$item->image->landscape) }}"><i class="lab la-pinterest"></i></a>
                                         </li>
                                     </ul>
                                 </div>
@@ -90,25 +79,19 @@
                     <div class="product-tab mt-40">
                         <ul class="nav nav-tabs" role="tablist">
                             <li class="nav-item">
-                                <a class="nav-link active" id="product-tab-desc" data-bs-toggle="tab"
-                                   href="#product-desc-content" role="tab" aria-controls="product-desc-content"
-                                   aria-selected="true">@lang('Description')</a>
+                                <a class="nav-link active" id="product-tab-desc" data-bs-toggle="tab" href="#product-desc-content" role="tab" aria-controls="product-desc-content" aria-selected="true">@lang('Description')</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" id="product-tab-team" data-bs-toggle="tab"
-                                   href="#product-team-content" role="tab" aria-controls="product-team-content"
-                                   aria-selected="false">@lang('Team')</a>
+                                <a class="nav-link" id="product-tab-team" data-bs-toggle="tab" href="#product-team-content" role="tab" aria-controls="product-team-content" aria-selected="false">@lang('Team')</a>
                             </li>
                         </ul>
                         <div class="tab-content">
-                            <div class="tab-pane fade show active" id="product-desc-content" role="tabpanel"
-                                 aria-labelledby="product-tab-desc">
+                            <div class="tab-pane fade show active" id="product-desc-content" role="tabpanel" aria-labelledby="product-tab-desc">
                                 <div class="product-desc-content">
                                     {{ __($item->description) }}
                                 </div>
                             </div>
-                            <div class="tab-pane fade fade" id="product-team-content" role="tabpanel"
-                                 aria-labelledby="product-tab-team">
+                            <div class="tab-pane fade fade" id="product-team-content" role="tabpanel" aria-labelledby="product-tab-team">
                                 <div class="product-desc-content">
                                     <ul class="team-list">
                                         <li><span>@lang('Director'):</span> {{ __($item->team->director) }}</li>
@@ -130,27 +113,20 @@
                                     @php
                                         $status = checkLockStatus($episode, $userHasSubscribed, $hasSubscribedItem);
                                     @endphp
-                                    <div
-                                        class="widget-item widget-item__overlay d-flex align-items-center justify-content-between"
-                                        data-img="{{ getImage(getFilePath('episode') . '/' . $episode->image) }}">
-                                        <div
-                                            class="widget-item__content d-flex align-items-center audio-small flex-wrap">
+                                    <div class="widget-item widget-item__overlay d-flex align-items-center justify-content-between" data-img="{{ getImage(getFilePath('episode') . '/' . $episode->image) }}">
+                                        <div class="widget-item__content d-flex align-items-center audio-small flex-wrap">
                                             <div class="widget-thumb">
                                                 <a href="{{ route('listen', [$item->slug, $episode->id]) }}">
-                                                    <img
-                                                        src="{{ getImage(getFilePath('episode') . '/' . $episode->image) }}"
-                                                        alt="audio">
+                                                    <img src="{{ getImage(getFilePath('episode') . '/' . $episode->image) }}" alt="audio">
                                                 </a>
                                             </div>
                                             <div class="widget-content">
                                                 <h4 class="title">{{ __($episode->title) }}</h4>
                                                 <div class="widget-btn">
                                                     @if ($status)
-                                                        <a class="custom-btn"
-                                                           href="{{ route('listen', [$item->slug, $episode->id]) }}">@lang('Play Now')</a>
+                                                        <a class="custom-btn" href="{{ route('listen', [$item->slug, $episode->id]) }}">@lang('Play Now')</a>
                                                     @else
-                                                        <a class="custom-btn"
-                                                           href="{{ route('user.login') }}">@lang('Subscribe to listen')</a>
+                                                        <a class="custom-btn" href="{{ route('user.login') }}">@lang('Subscribe to listen')</a>
                                                     @endif
                                                 </div>
                                             </div>
@@ -188,20 +164,14 @@
                     <div class="col-xl-3 col-lg-4 col-sm-6 mb-30">
                         <div class="audio-item">
                             <div class="audio-item__img">
-                                <img
-                                    src="{{ getImage(getFilePath('item_portrait') . '/' . $relatedItem->image->portrait) }}"
-                                    alt="audio">
+                                <img src="{{ getImage(getFilePath('item_portrait') . '/' . $relatedItem->image->portrait) }}" alt="audio">
                             </div>
                             <div class="audio-item__content">
-                                <h3 class="title"><a
-                                        href="{{ route('preview.audio', $relatedItem->id) }}">{{ __($relatedItem->title) }}</a>
-                                </h3>
+                                <h3 class="title"><a href="{{ route('preview.audio', $relatedItem->id) }}">{{ __($relatedItem->title) }}</a></h3>
                                 <p>{{ strLimit(strip_tags($relatedItem->description), 150) }}</p>
-                                <div
-                                    class="audio-widget-area d-flex align-items-center justify-content-between flex-wrap">
+                                <div class="audio-widget-area d-flex align-items-center justify-content-between flex-wrap">
                                     <span class="audio-widget"><i class="lar la-star text--warning"></i> {{ getAmount($relatedItem->ratings) }}</span>
-                                    <span class="audio-widget"><i
-                                            class="lar la-eye text--danger"></i> {{ getAmount($relatedItem->view) }} @lang('views')</span>
+                                    <span class="audio-widget"><i class="lar la-eye text--danger"></i> {{ getAmount($relatedItem->view) }} @lang('views')</span>
                                 </div>
                             </div>
                         </div>
@@ -219,3 +189,57 @@
     </script>
 @endpush
 
+
+<style>
+    .audio-item {
+    position: relative;
+    overflow: hidden;
+}
+
+.audio-background {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    background-position: center;
+    filter: blur(10px);
+    z-index: -2;
+}
+
+.audio-mask {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.6);
+    z-index: -1;
+}
+
+.plyr--full-ui input[type="range"] {
+    --plyr-color-main: #ff0;
+}
+
+.plyr__progress input[type="range"] {
+    background: url('/path-to-your-equalizer-image/equalizer-waves.png') no-repeat center;
+    background-size: cover;
+    border: none;
+    height: 10px;
+    outline: none;
+    opacity: 0.7;
+}
+
+.plyr__control--overlaid {
+    background: rgba(0, 0, 0, 0.8);
+    padding: 10px;
+    border-radius: 50%;
+    transition: background 0.3s;
+}
+
+.plyr__control--overlaid:hover {
+    background: rgba(0, 0, 0, 1);
+}
+
+    </style>
