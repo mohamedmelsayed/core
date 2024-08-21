44d22d04441c6559726fc0644a68a7a33bab8278
Mohamed <mohamedosmanelsayed@yahoo.com>
2024-08-21 17:43:04 +0400

    Update SiteController.php

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -193,7 +193,7 @@ class SiteController extends Controller
 
         if ($item->item_type == Status::EPISODE_ITEM) {
             $episodes = Episode::hasVideo()->with(['video', 'item'])->where('item_id', $item->id)->get();
-            $relatedItems = $this->relatedItems($item->id, Status::EPISODE_ITEM);
+            $relatedItems = $this->relatedItems($item->id, Status::EPISODE_ITEM,$item->keywords);
             $relatedAudios = $this->relatedAudios($item->id, Status::EPISODE_ITEM);
             $pageTitle = 'Episode Details';
 
@@ -225,8 +225,8 @@ class SiteController extends Controller
             $this->storeVideoReport($item->id);
 
             $pageTitle = 'Movie Details';
-            $relatedAudios = $this->relatedAudios($item->id, Status::EPISODE_ITEM);
-            $relatedItems = $this->relatedItems($item->id, Status::SINGLE_ITEM);
+            $relatedAudios = $this->relatedAudios($item->id, Status::SINGLE_ITEM);
+            $relatedItems = $this->relatedItems($item->id, Status::SINGLE_ITEM,$item->keywords);
             $episodes = [];
             $video = $item->video;
             $checkWatchEligable = $this->checkWatchEligableItem($item, $userHasSubscribed);
