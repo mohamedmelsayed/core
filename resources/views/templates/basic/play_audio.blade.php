324fa3e94e978d60f05cfba383a0448de54d3f38
Mohamed <mohamedosmanelsayed@yahoo.com>
2024-08-23 10:54:07 +0400

    related in audio play

--- resources/views/templates/basic/play_audio.blade.php
+++ resources/views/templates/basic/play_audio.blade.php
@@ -123,7 +123,7 @@
         <div class="row">
             <div class="col-xl-12">
                 <div class="section-header">
-                    <h2 class="section-title">@lang('Related Items')</h2>
+                    <h2 class="section-title">@lang('Related Videos')</h2>
                 </div>
             </div>
         </div>
@@ -141,7 +141,37 @@
                             @endif
                             <div class="movie-thumb-overlay">
                                 
-                                <a class="video-icon" href="{{ route('watch', $related->id) }}"><i class="fas fa-play"></i></a>
+                                <a class="video-icon" href="{{$related->is_audio?route('preview.audio', $related->slug) :route('watch', $related->slug) }}"><i class="fas fa-play"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12">
+                <div class="section-header">
+                    <h2 class="section-title">@lang('Related Audios')</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center mb-30-none">
+
+            @foreach($relatedItems as $related)
+                <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-xs-6 mb-30">
+                    <div class="movie-item">
+                        <div class="movie-thumb">
+                            <img src="{{ getImage(getFilePath('item_portrait').'/'.$related->image->portrait) }}" alt="movie">
+                            @if($related->item_type == 1 && $related->version == 0)
+                                <span class="movie-badge">@lang('Free')</span>
+                            @elseif($related->item_type == 3)
+                                <span class="movie-badge">@lang('Trailer')</span>
+                            @endif
+                            <div class="movie-thumb-overlay">
+                                
+                                <a class="video-icon" href="{{$related->is_audio?route('preview.audio', $related->slug) :route('watch', $related->slug) }}"><i class="fas fa-play"></i></a>
                             </div>
                         </div>
                     </div>
