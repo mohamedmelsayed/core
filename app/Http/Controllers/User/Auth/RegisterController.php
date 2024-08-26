dbb48e93b52967cce0abe0f7fda2615d80d6a9cf
Mohamed <mohamedosmanelsayed@yahoo.com>
2024-08-26 23:03:15 +0400

    str class

--- app/Http/Controllers/User/Auth/RegisterController.php
+++ app/Http/Controllers/User/Auth/RegisterController.php
@@ -10,6 +10,7 @@ use App\Models\User;
 use App\Models\UserLogin;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
