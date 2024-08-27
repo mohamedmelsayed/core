c1fec53cc6e76e9144b9e25aa968a3783841fcf6
Mohamed <mohamedosmanelsayed@yahoo.com>
2024-08-27 07:28:06 +0400

    allow user to skiip sms verification

--- resources/views/templates/basic/user/auth/authorization/sms.blade.php
+++ resources/views/templates/basic/user/auth/authorization/sms.blade.php
@@ -1,43 +1,49 @@
 @extends($activeTemplate . 'layouts.frontend')
 @section('content')
-    <section class="account-section bg-overlay-black ptb-80">
-        <div class="container">
-            <div class="d-flex justify-content-center">
-                <div class="verification-code-wrapper">
-                    <div class="verification-area">
-                        <form class="submit-form" action="{{ route('user.verify.mobile') }}" method="POST">
-                            @csrf
-                            <p>@lang('A 6 digit verification code sent to your mobile number') : +{{ showMobileNumber(auth()->user()->mobile) }}</p>
-                            @include($activeTemplate . 'partials.verification_code')
-                            <div class="mb-3">
-                                <button class="btn btn--base w-100" type="submit">@lang('Submit')</button>
-                            </div>
-                            <p>
-                                @lang('If you don\'t get any code'), <a class="forget-pass text--base" href="{{ route('user.send.verify.code', 'phone') }}"> @lang('Try again')</a>
-                            </p>
-                            @if ($errors->has('resend'))
-                                <br />
-                                <small class="text--danger">{{ $errors->first('resend') }}</small>
-                            @endif
-                        </form>
-                    </div>
+<section class="account-section bg-overlay-black ptb-80">
+    <div class="container">
+        <div class="d-flex justify-content-center">
+            <div class="verification-code-wrapper">
+                <div class="verification-area">
+                    <form class="submit-form" action="{{ route('user.verify.mobile') }}" method="POST">
+                        @csrf
+                        <p>@lang('A 6 digit verification code sent to your mobile number') : +{{ showMobileNumber(auth()->user()->mobile) }}</p>
+                        @include($activeTemplate . 'partials.verification_code')
+                        <div class="mb-3 d-flex justify-content-between">
+                            <button class="btn btn--base w-45" type="submit">@lang('Submit')</button>
+                            <a href="{{ route('home') }}" class="btn btn--base w-45">@lang('Skip')</a>
+                        </div>
+                        <p>
+                            @lang('If you don\'t get any code'), <a class="forget-pass text--base" href="{{ route('user.send.verify.code', 'phone') }}"> @lang('Try again')</a>
+                        </p>
+                        @if ($errors->has('resend'))
+                            <br />
+                            <small class="text--danger">{{ $errors->first('resend') }}</small>
+                        @endif
+                    </form>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 @endsection
+
 @push('style')
-    <style>
-        .verification-code-wrapper {
-            border: 1px solid #3e2d34;
-        }
+<style>
+    .verification-code-wrapper {
+        border: 1px solid #3e2d34;
+    }
 
-        .verification-code::after {
-            background-color: #11141d;
-        }
+    .verification-code::after {
+        background-color: #11141d;
+    }
 
-        .verification-code span {
-            border: solid 1px #3e2d34;
-        }
-    </style>
+    .verification-code span {
+        border: solid 1px #3e2d34;
+    }
+
+    .btn--base.w-45 {
+        width: 45%;
+    }
+</style>
 @endpush
