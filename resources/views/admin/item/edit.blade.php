9b0003f6f36cd28fbe1582aa2dd8217a3107c600
Mohamed <mohamedosmanelsayed@yahoo.com>
2024-08-29 23:16:08 +0400

    yes no <<

--- resources/views/admin/item/edit.blade.php
+++ resources/views/admin/item/edit.blade.php
@@ -83,16 +83,16 @@
                             <label>@lang('Item Type')</label>
                             <select class="form-control" name="is_audio">
                                 <option value="">@lang('Select One')</option>
-                                <option value="0"  @selected($item->is_audio == 0)>>@lang('Video')</option>
-                                <option value="1"  @selected($item->is_audio == 1)>>@lang('Audio')</option>
+                                <option value="0"  @selected($item->is_audio == 0)>@lang('Video')</option>
+                                <option value="1"  @selected($item->is_audio == 1)>@lang('Audio')</option>
                             </select>
                         </div>
                         <div class="form-group col-md-3 is_audio">
                                 <label>@lang('Stream Item')</label>
                                 <select class="form-control" name="is_stream">
                                     <option value="">@lang('Select One')</option>
-                                    <option value="0"  @selected($item->is_stream == Status::NO)>>@lang('Normal')</option>
-                                    <option value="1"  @selected($item->is_stream == Status::YES)>>@lang('Live Stream')</option>
+                                    <option value="0"  @selected($item->is_stream == Status::NO)>@lang('Normal')</option>
+                                    <option value="1"  @selected($item->is_stream == Status::YES)>@lang('Live Stream')</option>
                                 </select>
                             </div>
                         <div class="row d-none" id="rentalArea">
