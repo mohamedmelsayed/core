9f87406e3ad18140d27c594fc6cd9a40f39c26dd
Mohamed <mohamedosmanelsayed@yahoo.com>
2024-10-08 11:55:41 +0300

    Update SiteController.php

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -351,7 +351,7 @@ class SiteController extends Controller
         });
 
         // Apply the orderBy before calling get()
-        $items = $query->orderBy('id', 'desc')->get();
+        $items = $query->sortBy('id', 'desc')->get();
 
         // Filter items to return only those that have at least 2 matching keywords
         $filteredItems = $items->filter(function ($item) use ($keywordsArray) {
