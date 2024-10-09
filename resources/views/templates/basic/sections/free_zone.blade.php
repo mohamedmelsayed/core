dbbc3aff49ac46d19eba2e3d5ddb292f6a9c755d
Mohamed <mohamedosmanelsayed@yahoo.com>
2024-10-09 20:16:13 +0300

    Update free_zone.blade.php

--- resources/views/templates/basic/sections/free_zone.blade.php
+++ resources/views/templates/basic/sections/free_zone.blade.php
@@ -9,16 +9,37 @@
         </div>
         <div class="row justify-content-center mb-30-none">
             @foreach ($frees as $free)
-                <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-xs-6 mb-30">
-                    <div class="movie-item">
-                        <div class="movie-thumb">
-                            <img class="lazy-loading-img" data-src="{{ getImage(getFilePath('item_portrait') . '/' . $free->image->portrait) }}" src="{{ asset('assets/global/images/lazy.png') }}" alt="movie">
-                            <div class="movie-thumb-overlay">
-                                <a class="video-icon" href="{{ route('watch', $free->slug) }}"><i class="fas fa-play"></i></a>
-                            </div>
+            <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 col-xs-6 mb-30">
+                <div class="movie-item">
+                    <div class="movie-thumb">
+                        <img src="{{ getImage(getFilePath('item_portrait') . '/' . $free->image->portrait) }}" alt="movie">
+
+                        <!-- Display "Paid" if the item is not free with a yellow badge -->
+                        @if ($free->version != 0)
+                        <span class="movie-badge" style="background-color: yellow; color: black;">@lang('Paid')</span>
+                        @else
+                        <span class="movie-badge">@lang('Free')</span>
+                        @endif
+
+                        <!-- Display Font Awesome icon based on is_audio -->
+                        <span class="media-type" style="background-color: #000; color: #fff; padding: 3px 5px; border-radius: 3px;">
+                            @if($free->is_audio)
+                                <i class="fas fa-headphones"></i> <!-- Audio Icon -->
+                            @else
+                                <i class="fas fa-video"></i> <!-- Video Icon -->
+                            @endif
+                        </span>
+
+                        <div class="movie-thumb-overlay">
+                            <a class="video-icon" href="{{ $free->is_audio ? route('preview.audio', $free->slug) : route('watch', $free->slug) }}">
+                                <i class="fas fa-play"></i>
+                            </a>
                         </div>
                     </div>
                 </div>
+            </div>
+
+
             @endforeach
         </div>
     </div>
