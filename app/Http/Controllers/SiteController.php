a1757785c76668bc88b2a87ba44f162fab6d1ed2
Mohamed <mohamedosmanelsayed@yahoo.com>
2024-10-10 07:10:06 +0300

    Update SiteController.php

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -162,7 +162,7 @@ class SiteController extends Controller
         if ($request->sectionName == 'end') {
             return response('end');
         }
-        $items = Item::scopeHasVideoOrAudio();
+        $items = Item::HasVideoOrAudio();
         dd($items);
         if ($request->sectionName == 'recent_added') {
             $data['recent_added'] = (clone $items)->where('item_type', Status::SINGLE_ITEM)->orderBy('id', 'desc')->limit(18)->get();
