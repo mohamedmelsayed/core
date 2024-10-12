ec7f96a07cdde569fdf1fb574e3988fa8a122f80
Mohamed <mohamedosmanelsayed@yahoo.com>
2024-10-12 21:06:29 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Route::controller('SiteController')->name('wishlist.')->prefix('wishlist')->grou
 
 Route::controller('SiteController')->group(function () {
     Route::get('cron', 'cron')->name('cron');
+    Route::get('/playlist/{id}',  'showPlaylist')->name('playlist.show');
 
     Route::get('live-stream/{slug}', 'watchLive')->name('watch.live');
     Route::get('live-tv', 'liveTelevision')->name('live.tv');
