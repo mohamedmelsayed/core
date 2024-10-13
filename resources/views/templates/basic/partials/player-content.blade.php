fa47592b684c5b4ae3d9df6711a864de2c1b803d
Mohamed <mohamedosmanelsayed@yahoo.com>
2024-10-13 23:00:10 +0300

    Update player-content.blade.php

--- resources/views/templates/basic/partials/player-content.blade.php
+++ resources/views/templates/basic/partials/player-content.blade.php
@@ -1,7 +1,28 @@
-@if ($item->is_audio && $item->audio)
-    @include($activeTemplate . 'partials.audio-player', ['item' => $item,'seoContents'=>$seoContents])
-@elseif ($item->video)
-    @include($activeTemplate . 'partials.video-player', ['item' => $item, 'subtitles' => $subtitles, 'adsTime' => $adsTime, 'watchEligable' => $watchEligable,'seoContents'=>$seoContents])
-@else
-    <p>@lang('No media available in this playlist to play.')</p>
-@endif
+    @if ($item)
+        @if ($item->is_audio)
+            <!-- Audio Player Widget -->
+            @if ($item->audio)
+                <!-- Include Audio Player Partial -->
+                @include($activeTemplate . 'partials.audio-player', ['item' => $item])
+            @else
+                <!-- Fallback message for missing audio content -->
+                <p>@lang('Audio content is not available for this item.')</p>
+            @endif
+        @else
+            <!-- Video Player Widget -->
+            @if ($item->video)
+                <!-- Include Video Player Partial -->
+                @include($activeTemplate . 'partials.video-player', [
+                    'item' => $item,
+                    'subtitles' => $subtitles,
+                    'adsTime' => $adsTime,
+                    'watchEligable' => $watchEligable,
+                ])
+            @else
+                <p>@lang('Video content is not available for this item.')</p>
+            @endif
+        @endif
+    @else
+        <!-- Fallback message if no item is selected -->
+        <p>@lang('No media available in this playlist to play.')</p>
+    @endif
