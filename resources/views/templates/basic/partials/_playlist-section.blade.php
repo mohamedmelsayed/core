666c748a0e380d7874d9dc16a87314ac8691715c
Mohamed <mohamedosmanelsayed@yahoo.com>
2024-10-14 14:10:49 +0300

    Update _playlist-section.blade.php

--- resources/views/templates/basic/partials/_playlist-section.blade.php
+++ resources/views/templates/basic/partials/_playlist-section.blade.php
@@ -1,17 +1,26 @@
-<!-- Playlists containing this item -->
-@if ($playlists->isNotEmpty())
-    <h4 class="playlist-heading">@lang('Playlists Containing this Item')</h4>
-    <div class="playlists-container">
-        @foreach ($playlists as $playlist)
-            <div class="playlist-item">
-                <img src="{{ getImage(getFilePath('item_portrait') . '/' . $playlist->cover_image) }}"
-                    alt="{{ $playlist->title }}" class="playlist-thumb">
-                <h5 class="playlist-title">{{ $playlist->title }}</h5>
-                <a href="{{ route('playlist.play', $playlist->id) }}" class="playlist-btn">@lang('View Playlist')</a>
+
+@if($playlists->isNotEmpty())
+<div class="playlist-section mt-4">
+    <h4 class="section-title">@lang('Playlists Containing this Item')</h4>
+    <div class="row">
+        @foreach($playlists as $playlist)
+            <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-4">
+                <div class="playlist-item">
+                    <div class="playlist-thumb">
+                        <a href="{{ route('playlist.play', $playlist->id) }}" >
+                        <img src="{{ getImage(getFilePath('item_portrait') . '/' . $playlist->cover_image) }}"
+                            alt="{{ $playlist->title }}" class="img-fluid">
+                        </a>
+                    </div>
+                    <div class="playlist-content">
+                        <a href="{{ route('playlist.play', $playlist->id) }}" >
+                            <h5 class="playlist-title">{{ $playlist->title }}</h5>
+
+                        </a>
+                    </div>
+                </div>
             </div>
         @endforeach
     </div>
+</div>
 @endif
-
-
-
