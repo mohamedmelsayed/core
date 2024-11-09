07975586e96c14813c7aa6d7905b5de8682d5dda
Mohamed <mohamedosmanelsayed@yahoo.com>
2024-11-09 22:46:11 +0400

    api play list by categoryId

--- app/Http/Controllers/Api/FrontendController.php
+++ app/Http/Controllers/Api/FrontendController.php
@@ -493,12 +493,20 @@ class FrontendController extends Controller
     }
 
     public function playlists(Request $request){
+        $category_id = $request->query('category_id');
+
         $allPlaylists = Playlist::whereHas('items', function ($query) {
             $query->where(function ($q) {
                 $q->whereHas('video')->orWhereHas('audio');
             });
+
+            if ($category_id) {
+                $query->where('sub_category_id', $category_id);
+            }
+
         })->limit(12)->get();
         $notify[] = 'Play Lists';
+        $remark   = 'play_lists';
 
         return response()->json([
             'remark'  => $remark,
