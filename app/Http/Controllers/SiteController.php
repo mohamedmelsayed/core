14a243b403f2b751fde512c6bbab47dfc86d25be
Mohamed Osman <mohamedosmanelsayed@yahoo.com>
2024-12-07 10:01:11 +0400

    Update SiteController.php

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -709,7 +709,7 @@ class SiteController extends Controller
         $hasStream =false;
         foreach ($items as $item) {
             # code...
-            if($item->is_stream,1){
+            if($item->is_stream,true){
                 $hasStream=true;
                 break;
             }
@@ -758,7 +758,7 @@ class SiteController extends Controller
 
         foreach ($items as $item) {
             # code...
-            if($item->is_stream,1){
+            if($item->is_stream,true){
                 $hasStream=true;
                 break;
             }
