0d30a7b2d47d1475a73d03361856c50f15a7d80c
Mohamed Osman <mohamedosmanelsayed@yahoo.com>
2025-01-08 17:33:12 +0200

    dashboard

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -87,14 +87,9 @@ class UserController extends Controller {
 
         $translate = ContentTranslation::where("item_id", $item->id)->where("language", $lang)->first();
         if ($translate != null) {
-            $item->tags = $translate->translated_tags ?? $item->tags;
             $item->title = $translate->translated_title;
             $item->description = $item->description;
-        } else {
-            $item->tags = $item->meta_keywords ?? [];
-            $item->description = $item->description;
-        }
-        $item->meta = $item->meta ?? json_decode($item->meta);
+        } 
         return $item;
     }
 
